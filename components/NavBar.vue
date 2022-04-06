18ff2e839583148259b64378e58733d591eda48a
suguru-3u <suguru.13579515@gmail.com>
2022-04-06 09:49:12 +0900

    study nuxt

---
+++ components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">Abouot</nuxt-link>
+    </nav>
+  </div>
+</template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>記事一覧</h1>
+    <!-- <p v-if="$fetchState.pending">Fetching posts...</p> -->
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  //   async asyncData({ $axios }) {
+  //     const posts = await $axios.$get(
+  //       "https://jsonplaceholder.typicode.com/posts/"
+  //     );
+  //     return { posts };
+  //   },
+  //   data() {
+  //     return {
+  //       posts: [],
+  //     };
+  //   },
+  //   async fetch() {
+  //     const posts = await this.$axios.$get(
+  //       "https://jsonplaceholder.typicode.com/posts/"
+  //     );
+  //     this.posts = posts;
+  //   },
+  //   fetchOnServer: false,
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async mounted() {
+    const posts = await this.$axios.$get(
+      "https://jsonplaceholder.typicode.com/posts/"
+    );
+    this.posts = posts;
+  },
+};
+</script>
